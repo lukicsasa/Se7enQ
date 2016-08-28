f781cffee4b311667c8f4d25521f187a787280a7
David Tadić <david.tadic@hotmail.com>
2016-08-28 20:14:05 +0200

    postdeployment script

---
+++ Se7enQ.Sql/Se7enQPostDeployment.sql
@@ -0,0 +1,14 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+INSERT INTO [dbo].[User] ([Username],[Password],[FirstName],[LastName],[DateCreated],[Email],[Playing],[ImageUrl])
+VALUES ('admin','admin','Admin','Admin',GETDATE(),'admin@admin.com',0,null)
