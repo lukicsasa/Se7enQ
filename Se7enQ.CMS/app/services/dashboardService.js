3f7084e074bf23cb381c2e7655ac71159e4fe460
david <david.tadic1@gmail.com>
2017-01-16 16:21:44 +0100

    fixes

---
+++ Se7enQ.CMS/app/services/dashboardService.js
@@ -0,0 +1,16 @@
+﻿app.factory('dashboardService', function ($http, $state, $q, $rootScope, toastService) {
+    var factory = {};
+
+    factory.getUsers = function () {
+        return $http({
+            url: config.baseAddress + 'admin/getUsers',
+            method: 'GET'
+        }).then(function (response) {
+            return response.data;
+        }, function (error) {
+            return $q.reject(error.data.message);
+        });
+    };
+
+    return factory;
+});
